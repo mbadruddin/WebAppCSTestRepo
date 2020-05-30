66e5f681e8890512f5d0ecba1ae666becc1b475b
Mohammed Badaruddin <Badaruddin@ivq.com>
2020-05-30 12:10:09 +0300

    Add project files.

---
+++ WebAppCSTestRepo/TestWebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebAppCSTestRepo
+{
+    public partial class TestWebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            const string V = "this is on click event ";
+            Label1.Text = V;
+        }
+    }
+}
